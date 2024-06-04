d1294511737840d45ce509ffaa62543ce2022964
Ahmedsaleh1234 <a12661826@gmail.com>
2024-06-04 12:44:22 +0300

    hello react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+function Hello(){
+function alt(){
+  alert('hello')
+}
+return(  
+<button onClick={alt}>click me</button>
+)
+}
+function State(){
+  const [count, setcount] = useState(0)
+  function st(){
+    setcount(count + 1);
+  }
+  return(
+    <button onClick={st}> click me {count}</button>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Hello />
+    <br/>
+    <br/>
+    <State />
     </div>
   );
 }
